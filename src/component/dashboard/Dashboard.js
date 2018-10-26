ec2d2bac16ffa1dae62908014e86ecfadbcb4760
dnljsn <daniel@kalood.com>
2018-10-26 10:14:14 -0600

    React Setup

---
+++ src/component/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Product from '../product/Product';
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                Dashboard
+                <Product />
+            </div>
+        )
+    }
+}
